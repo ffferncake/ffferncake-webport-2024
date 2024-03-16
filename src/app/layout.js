9d490e4ae62999847cec39e7c36030ed18eb0ff6
ffferncake <59507098+ffferncake@users.noreply.github.com>
2024-03-16 22:31:57 +0900

    add head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
       <link rel="icon" type="image/png" href="/images/carrot.png" />
+      </head>
       <body className={poppins.className}>{children}</body>      
     </html>
   );
